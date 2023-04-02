1e0cf217d325452f316ba03358529d32f7261227
dvolokonsky <110537533+dvolokonsky@users.noreply.github.com>
2023-04-02 20:34:15 +0300

    Update
    (Nothing works, again)

--- interactive.js
+++ interactive.js
@@ -1,36 +1,41 @@
-const dots = document.querySelectorAll(".dot");
-let slideIndex = 1;
-showSlides(slideIndex);
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryCarousel = document.querySelector('.gallery-carousel');
+const galleryImages = document.querySelectorAll('.gallery-image');
+const galleryDots = document.querySelectorAll('.gallery-dot');
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+let imageIndex = 0;
+let translateX = 0;
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+// Set active dot on page load
+galleryDots[imageIndex].classList.add('active');
 
-function showSlides(n) {
-  let i;
-  const slides = document.querySelectorAll(".mySlides");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].classList.remove("active");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].classList.add("active");
-}
-
-dots.forEach((dot, index) => {
-  dot.addEventListener("click", () => {
-    currentSlide(index + 1);
+// Change image and dot when dot is clicked
+galleryDots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    imageIndex = index;
+    translateX = -imageIndex * galleryImages[imageIndex].clientWidth;
+    galleryCarousel.style.transform = `translateX(${translateX}px)`;
+    updateActiveDot();
   });
 });
+
+// Change image and dot every 5 seconds
+setInterval(() => {
+  imageIndex++;
+  if (imageIndex > galleryImages.length - 1) {
+    imageIndex = 0;
+    translateX = 0;
+  } else {
+    translateX -= galleryImages[imageIndex].clientWidth;
+  }
+  galleryCarousel.style.transform = `translateX(${translateX}px)`;
+  updateActiveDot();
+}, 5000);
+
+// Update active dot
+function updateActiveDot() {
+  galleryDots.forEach(dot => {
+    dot.classList.remove('active');
+  });
+  galleryDots[imageIndex].classList.add('active');
+}
